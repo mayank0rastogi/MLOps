c90224e10540aefbf75443506fe07f4d277c993f
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-07-16 14:38:35 +0530

    Image-Classification Using Docker

---
+++ Deploy Models with TensorFlow Serving and Flask/ImageClassification-Flask(tensorflow-serving) Using Docker/inference.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+import json
+import requests
+
+SIZE=128
+MODEL_URI='http://localhost:8501/v1/models/pets:predict'
+CLASSES=['CAT','DOG']
+
+def get_predictions(image_path):
+
+    image=tf.keras.preprocessing.image.load_img(image_path,target_size=(SIZE,SIZE))
+    #convert the image to a numpy array
+    image=tf.keras.preprocessing.image.img_to_array(image)
+    #use the mobile-net preprocess input
+    image=tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    image=np.expand_dims(image,axis=0)
+
+
+
+    data=json.dumps({
+    'instances':image.tolist()
+
+    })
+
+    response=requests.post(MODEL_URI,data=data.encode())
+    result=json.loads(response.text)
+    # print(result)
+    prediction=np.squeeze(result['predictions'][0])
+    class_name= CLASSES[int(prediction>0.5)]
+    return class_name
+
+# get_predictions('anon.jpg')
